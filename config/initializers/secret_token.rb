becbf7374870cb827a76e15a8c5e93ef7181a1cc
Amol Atigre <amol@anomaly.co.in>
2012-01-16 16:06:44 +0430

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery1::Application.config.secret_token = 'eb1bf0b4f5578750e9aff53063226fdf3e8879876eec216508afc2cd34fa6cb56c9f43f4537a362ded6cf237248d5d09a9e38a6f49e3692e37a42a42bd85d127'
